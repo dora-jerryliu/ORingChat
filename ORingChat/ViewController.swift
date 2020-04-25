32da9f57461f56a08f3ef7dc56f849bbc3b0d37d
Jerry Liu <jerryliu@oringnet.com>
2020-04-25 09:44:13 +0800

    Initial Commit

---
+++ ORingChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ORingChat
+//
+//  Created by jerryliu on 2020/4/25.
+//  Copyright © 2020 oring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
